a328f6fb9652f10a377475fd3d8194814822b83c
flash <arielivandiaz@gmail.com>
2017-12-01 01:43:12 -0300

    LFOVS update 1.68

--- results/LFOVS_v2.sh
+++ results/LFOVS_v2.sh
@@ -1,7 +1,7 @@
 sh clean.sh
 rm run.sh
 #VSUMM1
-python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -cus -g -lfvos -lfovs_s 0.39 -lfovs_n 0.95 -lfovs_d 0.19 -lfovs_t 30 -lfovs_e 3
+python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -cus -g -lfovs -lfovs_s 0.39 -lfovs_n 0.95 -lfovs_d 0.19 -lfovs_t 30 -lfovs_e 3
 
 sh run.sh
 
