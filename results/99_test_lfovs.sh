a014dc26713ade5c35f3edd514c1fbd72adfa65f
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2017-12-14 20:35:05 -0300

    local changes

--- results/99_test_lfovs.sh
+++ results/99_test_lfovs.sh
@@ -1,7 +1,7 @@
 sh clean.sh
 rm run.sh
 #VSUMM1
-python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -cus -g -lfovs -lfovs_s 0.1 -lfovs_n 0.975 -lfovs_d 0.15 -lfovs_t 5 -lfovs_e 15
+python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -cus -g -lfovs -lfovs_s 0.1 -lfovs_n 0.975 -lfovs_d 0.15 -lfovs_t 20 -lfovs_e 15
 
 sh run.sh
 
@@ -12,6 +12,5 @@ python ../tools/get_average/get_average.py output.csv averange.txt TUNNER
 cp output.txt DATA/output-"$(date +%d-%m-%H_%M_%S).txt"
 cp output.csv DATA/output-"$(date +%d-%m-%H_%M_%S).csv"
 cp averange.txt DATA/output-averange-"$(date +%d-%m-%H_%M_%S).csv"
-rm output.txt
-rm output.csv
-rm averange.txt
+
+
