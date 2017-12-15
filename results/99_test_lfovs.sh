928310b5f3af5ec33da0aba818fe8bb5ef40971b
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2017-12-14 21:38:44 -0300

    new results

--- results/99_test_lfovs.sh
+++ results/99_test_lfovs.sh
@@ -1,7 +1,7 @@
 sh clean.sh
 rm run.sh
 #VSUMM1
-python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -cus -g -lfovs -lfovs_s 0.05 -lfovs_n 0.98 -lfovs_d 0.15 -lfovs_t 20 -lfovs_e 60
+python ../tools/osm_utility/osm_utiity_out.py ../videos/VSUMM ../user_summaries/VSUMM ../auto_summaries/VSUMM1 -bhi -g -lfovs -lfovs_s 0.05 -lfovs_n 0.98 -lfovs_d 0.15 -lfovs_t 20 -lfovs_e 60
 
 sh run.sh
 
