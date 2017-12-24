6908517d22c5635d872cdacfd948589e94fca4b2
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2017-12-24 02:25:32 -0300

    lfovs tests0

--- results/run_cus_lfovs.sh
+++ results/run_cus_lfovs.sh
@@ -11,6 +11,5 @@ python ../tools/get_average/get_average.py output.csv averange.txt LFOVS_CUS
 cp output.txt DATA/output-"$(date +%d-%m-%H-%M-%S).txt"
 cp output.csv DATA/output-"$(date +%d-%m-%H-%M-%S).csv"
 cp averange.txt DATA/output-averange-"$(date +%d-%m-%H-%M-%S).csv"
-rm output.txt
-rm output.csv
+
 rm averange.txt
